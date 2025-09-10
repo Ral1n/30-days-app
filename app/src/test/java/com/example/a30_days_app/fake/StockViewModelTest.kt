e1ebd5523f6d9d9921a01a5c37b4ba2daf1158ff
Ral1n <alin.raduta24@gmail.com>
2025-09-10 20:33:20 +0300

    Test Dispatcher added

--- app/src/test/java/com/example/a30_days_app/fake/StockViewModelTest.kt
+++ app/src/test/java/com/example/a30_days_app/fake/StockViewModelTest.kt
@@ -1,11 +1,16 @@
 package com.example.a30_days_app.fake
 
+import com.example.a30_days_app.rules.TestDispatcherRule
 import com.example.a30_days_app.viewmodel.StockViewModel
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 
 class StockViewModelTest {
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
+
     @Test
     fun stockViewModel_getStockPrices_verifyStockUiStateSuccess() = runTest {
         val stockViewModel = StockViewModel(
